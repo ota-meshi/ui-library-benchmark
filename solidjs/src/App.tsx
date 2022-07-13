21640b5a2d7ee196f82b7076bb483d5ee71a76de
uhyo <uhyo@uhy.ooo>
2022-07-14 00:47:01 +0900

    solid

--- solidjs/src/App.tsx
+++ solidjs/src/App.tsx
@@ -1,26 +1,78 @@
-import { Component, createSignal, Index, useTransition } from "solid-js";
+import { Component, createEffect, createSignal, Index, on } from "solid-js";
 import classes from "./App.module.css";
 import { Item } from "./components/Item";
 import { SearchBox } from "./components/SearchBox";
 import { itemMap } from "./data/items";
+import { queueTask } from "./queueTask";
+
+type SearchQuery = {
+  prev: string;
+  current: string;
+  threshold: number;
+};
 
 const App: Component = () => {
   const [input, setInput] = createSignal("");
-  const [searchQuery, setSearchQuery] = createSignal("");
-  const [, startTransition] = useTransition();
+  const [searchQuery, setSearchQuery] = createSignal<SearchQuery>({
+    prev: "",
+    current: "",
+    threshold: 0,
+  });
+
+  let taskId = 0;
+  createEffect(
+    on(input, (input) => {
+      const t = ++taskId;
+      setSearchQuery({
+        prev: searchQuery().prev,
+        current: input,
+        threshold: 0,
+      });
+
+      let threshold = 0;
+      step();
+
+      function step() {
+        if (t !== taskId) {
+          return;
+        }
+        threshold += 100;
+        // console.log(t, threshold);
+        if (threshold < itemMap.size) {
+          setSearchQuery((query) => ({
+            ...query,
+            threshold,
+          }));
+          queueTask(step);
+        } else {
+          setSearchQuery({
+            prev: input,
+            current: input,
+            threshold,
+          });
+        }
+      }
+    })
+  );
 
   const onChange = (input: string) => {
     setInput(input);
-    startTransition(() => {
-      setSearchQuery(input.toLowerCase());
-    });
   };
 
   return (
     <>
       <div classList={{ [classes.pokemonList]: true }}>
         <Index each={Array.from(itemMap.keys())}>
-          {(id) => <Item id={id()} searchQuery={searchQuery()} />}
+          {(id, index) => (
+            <Item
+              id={id()}
+              searchQuery={
+                index < searchQuery().threshold
+                  ? searchQuery().current
+                  : searchQuery().prev
+              }
+            />
+          )}
         </Index>
       </div>
       <footer classList={{ [classes.footer]: true }}>
